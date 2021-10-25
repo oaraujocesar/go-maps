30a55e6df3262ddf0e132fc2ec9c834cc25f4336
César O. Araújo <oaraujocesar@gmail.com>
2021-10-25 19:04:49 -0300

    Create an empty map with a second approach

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#FF0000",
-		"green": "#00FF00",
-		"blue":  "#0000FF",
-	}
+
+	var colors map[string]string
+
+	// colors := map[string]string{
+	// 	"red":   "#FF0000",
+	// 	"green": "#00FF00",
+	// 	"blue":  "#0000FF",
+	// }
 
 	fmt.Println(colors)
 }
